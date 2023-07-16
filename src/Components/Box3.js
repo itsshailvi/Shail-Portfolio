34e450013ed92690cb711add7aa8ee1f7a387038
itsshailvi <itsshailvi@gmail.com>
2023-07-17 01:06:38 +0530

    changes in box3 and working on header menu

--- src/Components/Box3.js
+++ src/Components/Box3.js
@@ -3,10 +3,11 @@ import React from 'react'
 export const Box3 = () => {
   return (
     <div style={{background: '#FFD324', width: '66.7%' , position: 'relative', display: 'flex'}}>
+        <div style={{width: '33%'}}></div>
         <div>
-            
+            <h5> development is my passion </h5>
+            <h1 style={{fontSize:'65px', fontWeight:"800", fontFamily: "Montserrat", textTransform: 'capitalize'}}> Get to know me.</h1>
         </div>
-        <div></div>
     </div>
   )
 }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,22 +1,75 @@
 import React from 'react'
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+
+const options = [
+    'None',
+    'Atria',
+    'Callisto',
+    'Dione',
+    'Ganymede',
+    'Hangouts Call',
+    'Luna',
+    'Oberon',
+    'Phobos',
+    'Pyxis',
+    'Sedna',
+    'Titania',
+    'Triton',
+    'Umbriel',
+  ];
+  
+  const ITEM_HEIGHT = 48;
 
 export const Header = () => {
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
     return (
-        <div style={{display:'flex', justifyContent:'space-between', paddingRight: '100px', paddingTop: '40px' , paddingBottom: '0px', paddingLeft: '100px'}}>
+        <div style={{ display: 'flex', justifyContent: 'space-between', paddingRight: '100px', paddingTop: '40px', paddingBottom: '0px', paddingLeft: '100px' }}>
             <div>
-                <h1 style={{color: 'rgb(229, 224, 255)', background: 'FF8AAE'}}> @ShailviStart</h1>
+                <h1 style={{ color: '#FFB4B4', background: 'FF8AAE' }}> @ShailviStart</h1>
             </div>
             <div>
-            <IconButton
-                edge="start"
-                sx={{ mr: 2 }}
-                color="inherit"
-                aria-label="menu"
-            >
-                <MenuIcon sx={{ fontSize: 40 }} />
-            </IconButton>
+                <IconButton
+                    edge="start"
+                    sx={{ mr: 2 }}
+                    color="inherit"
+                    aria-label="menu"
+                    onClick={handleClick}
+                >
+                    <MenuIcon sx={{ fontSize: 40 }} />
+                </IconButton>
+                <Menu
+                    id="long-menu"
+                    MenuListProps={{
+                        'aria-labelledby': 'long-button',
+                    }}
+                    anchorEl={anchorEl}
+                    open={open}
+                    onClose={handleClose}
+                    PaperProps={{
+                        style: {
+                            maxHeight: ITEM_HEIGHT * 6,
+                            width: '18ch',
+                            left: '955px'
+                        },
+                    }}
+                >
+                    {options.map((option) => (
+                        <MenuItem key={option} selected={option === 'Pyxis'} onClick={handleClose}>
+                            {option}
+                        </MenuItem>
+                    ))}
+                </Menu>
+
 
             </div>
         </div>
