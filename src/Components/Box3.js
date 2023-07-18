a1a5361ac7408ddc9d5ef7cbd2407b3cf1eba8b6
itsshailvi <itsshailvi@gmail.com>
2023-07-19 00:40:48 +0530

    text changes

--- src/Components/Box3.js
+++ src/Components/Box3.js
@@ -5,8 +5,8 @@ export const Box3 = () => {
     <div style={{background: '#FFD324', width: '66.7%' , position: 'relative', display: 'flex'}}>
         <div style={{width: '33%'}}></div>
         <div>
-            <h5> development is my passion </h5>
-            <h1 style={{fontSize:'65px', fontWeight:"800",color: '#3A3A3A', textTransform: 'capitalize'}}> Get to know me.</h1>
+            <h5 style={{color: '#3A3A3A',textAlign: 'left',paddingTop: '20px', paddingBottom: '0px', marginBottom: '0px',fontFamily: 'Playfair Display',fontSize: '18px'}}> Development is my passion </h5>
+            <h1 style={{fontSize:'65px', fontWeight:"800",color: '#3A3A3A',textAlign: 'left', textTransform: 'capitalize', marginTop: '10px'}}> Get to <br/>know <br/>me.</h1>
         </div>
     </div>
   )
