64a18c746d5cb8f550d0587c430c9fdc22302fb9
itsshailvi <itsshailvi@gmail.com>
2023-07-20 22:06:59 +0530

    box3 css with image

--- src/Components/Box3.js
+++ src/Components/Box3.js
@@ -8,6 +8,7 @@ export const Box3 = () => {
             <h5 style={{color: '#3A3A3A',textAlign: 'left',paddingTop: '20px', paddingBottom: '0px', marginBottom: '0px',fontFamily: 'Playfair Display',fontSize: '18px'}}> Development is my passion </h5>
             <h1 style={{fontSize:'65px', fontWeight:"800",color: '#3A3A3A',textAlign: 'left', textTransform: 'capitalize', marginTop: '10px'}}> Get to <br/>know <br/>me.</h1>
         </div>
+        <img style={{width:"500px", height:"300px", top: "70px", left:"-295px", position: "absolute"}} src='https://library.elementor.com/designer-portfolio/wp-content/uploads/sites/68/2019/10/About.png'></img>
     </div>
   )
 }
