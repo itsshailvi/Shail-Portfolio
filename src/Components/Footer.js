e62af5e6abfe7b0657f8b7cf3a3b0d048900f4f2
itsshailvi <itsshailvi@gmail.com>
2023-07-24 00:22:54 +0530

    footer background added

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,6 +2,9 @@ import React from 'react'
 
 export const Footer = () => {
   return (
-    <div style={{minHeight: '285px', background:"#6f77f4", marginTop: "50px", borderStyle: "solid" , borderWidth : "0px 50px", borderColor: "white"}}>Footer</div>
+    <div style={{minHeight: '285px',  marginTop: "50px", borderStyle: "solid" , borderWidth : "0px 50px 50px 50px", borderColor: "white", display:'flex'}}>
+        <div style={{background:"#6f77f4", width:"65%"}}></div>
+        <div style={{background: '#FFD324', width: "35%"}}></div>
+    </div>
   )
 }
