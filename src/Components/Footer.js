66948b5969e4c55846e87a62fb0b86f5cebc0527
itsshailvi <itsshailvi@gmail.com>
2023-07-26 00:13:30 +0530

    icons set

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -24,13 +24,15 @@ export const Footer = () => {
         <div style={{ minHeight: '285px', marginTop: "50px", borderStyle: "solid", borderWidth: "0px 50px 50px 50px", borderColor: "white", display: 'flex' }}>
             <div style={{ background: "#6f77f4", width: "67%" }}></div>
             <div style={{ background: '#FFD324', width: "33%", display: "flex", justifyContent: "center", alignItems: "flex-start" , flexDirection: 'column',}}>
-                <div style={{paddingLeft: '15%', }}>
-                <div style={{}}>
-                    <h1 style={{ fontSize: '18px', fontFamily: "Montserrat", fontWeight: '600', color: "#3A3A3A" }}>Keep in Touch</h1>
+                <div style={{paddingLeft: '15%', display: "flex", flexDirection: "column", alignItems: "flex-start"}}>
+                <div style={{paddingLeft: '14px' }}>
+                    <h1 style={{ fontSize: '18px', fontFamily: "Montserrat", fontWeight: '600', color: "#3A3A3A" , marginBottom: "6px"}}>Keep in Touch</h1>
                 </div>
-                {/* {items.map((item)=>
-                    <div style={{padding: "5px"}}>{item.icon}<div/>
-                )}  */}
+                <div style={{display: "flex", flexDirection: "row"}}>
+                {items.map((item, index) => <div style={{padding: "0px 5px", fontSize: "24px", margin: "5px"}} key={index}>{item.icon}</div>
+                )}
+
+                </div>
 
                 </div>
             </div>
